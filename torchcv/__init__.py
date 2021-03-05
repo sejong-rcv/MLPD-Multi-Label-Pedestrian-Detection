771fd8a30b3dc65040ba9e6067155e5768d40f6d
tjkim <xown3197@naver.com>
2021-03-05 12:22:44 +0000

    update

--- torchcv/__init__.py
+++
@@ -1,4 +0,0 @@
-from torchcv import utils
-from torchcv import datasets
-#from torchcv import visualizations
-from torchcv import evaluations
--- torchcv/utils/tensorboard.py
+++
@@ -1,9 +0,0 @@
-import subprocess, atexit
-
-def run_tensorboard( jobs_dir, port=6006 ):
-    pid = subprocess.Popen( ['tensorboard', '--logdir', jobs_dir, '--host', '0.0.0.0', '--port', str(port)] )    
-    
-    def cleanup():
-    	pid.kill()
-
-    atexit.register( cleanup )
