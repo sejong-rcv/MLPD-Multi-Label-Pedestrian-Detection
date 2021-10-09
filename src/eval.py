57577a8d73a6b094b0cdf617baa2bfcbe304beec
Soonmin Hwang <jjang9hsm@gmail.com>
2021-10-09 06:23:40 +0000

    Clean-up inference codes

--- src/eval.py
+++ src/eval.py
@@ -1,214 +1,155 @@
-import sys, os, argparse, json, pdb, time, importlib
-
 from tqdm import tqdm
+from typing import Dict
+import argparse
+import config
+# import importlib
+import numpy as np
+import os
 
 import torch
 import torch.nn as nn
 
-import matplotlib.pyplot as plt
-from PIL import Image
-
-from datasets import KAISTPed, LoadBox
-
-from utils.coco import COCO
-from utils.eval_MR_multisetup import COCOeval
-from utils.utils import *
-
+from datasets import KAISTPed
 from utils.transforms import FusionDeadZone
 
-import config
-args = importlib.import_module('config').args
+# TODO(sohwang): why do we need this?
+# args = importlib.import_module('config').args
 
 
-def evaluate_coco(test_loader, model, rstFile=None):
+def run_inference(model_path: str, fdz_case: str) -> Dict:
+    """Load model and run inference
+
+    Load pretrained model and run inference on KAIST dataset with FDZ setting.
+
+    Parameters
+    ----------
+    model_path: str
+        Full path of pytorch model
+    fdz_case: str
+        Fusion dead zone case defined in utils/transforms.py:FusionDeadZone
+
+    Returns
+    -------
+    Dict
+        A Dict of numpy arrays (K x 5: xywh + score) for given image_id key
     """
-    Evaluate.
 
-    :param test_loader: DataLoader for test data
-    :param model: model
-    """
-    fig_test,  ax_test  = plt.subplots(figsize=(18,15))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.load(model_path)['model']
+    model = model.to(device)
 
-    data_time = AverageMeter()
-
-    # MLPD config
-    input_size = args.test.input_size
-    device = args.device
-
-    # Make sure it's in eval mode
+    model = nn.DataParallel(model)
     model.eval()
 
-    # Lists to store detected and true boxes, labels, scores
-    det_boxes = list()
-    det_labels = list()
-    det_scores = list()
-    true_boxes = list()
-    true_labels = list()
+    input_size = config.test.input_size
+    input_h, input_w = input_size
+    xyxy_scaler_np = np.array([[input_w, input_h, input_w, input_h]], dtype=np.float32)
 
-    #For CoCo
-    results = []
+    # Load dataloader for Fusion Dead Zone experiment
+    FDZ = [FusionDeadZone(fdz_case, tuple(input_size)) ]
+    config.test.img_transform.add(FDZ)
 
+    args = config.args
+    test_dataset = KAISTPed(args, condition="test")
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                                batch_size=config.test.batch_size * torch.cuda.device_count(),
+                                                num_workers=args.dataset.workers,
+                                                collate_fn=test_dataset.collate_fn,
+                                                pin_memory=True)
+    print(test_loader)
+
+    results = dict()
     with torch.no_grad():
-        # Batches
-        for i, (image_vis, image_lwir, boxes, labels, index) in enumerate(tqdm(test_loader, desc='Evaluating')):
-
-            image_vis = image_vis.to(device)
-            image_lwir = image_lwir.to(device)
-            
-            start = time.time()
-
-            # Forward prop.
-            predicted_locs, predicted_scores = model(image_vis, image_lwir)
-            data_time.update(time.time() - start)
-            
-            # Detect objects in SSD output
-            det_boxes_batch, det_labels_batch, det_scores_batch, det_bg_scores_batch = model.module.detect_objects(predicted_locs, predicted_scores,
-                                                                                       min_score=0.1, max_overlap=0.425,
-                                                                                       top_k=50)
-    
-            
-            for box_t, label_t, score_t, bg_score_t, ids in zip(det_boxes_batch ,det_labels_batch, det_scores_batch, det_bg_scores_batch, index):
-                for box, label, score, bg_score in zip(box_t, label_t, score_t, bg_score_t) :
-    
-                    bb = box.cpu().numpy().tolist()
-                    
-                    results.append( {\
-                                    'image_id': ids.item(), \
-                                    'category_id': label.item(), \
-                                    'bbox': [bb[0]*input_size[1], bb[1]*input_size[0], (bb[2]-bb[0])*input_size[1], (bb[3]-bb[1])*input_size[0]], \
-                                    'score': score.mean().item()} )
-
-    os.makedirs(args.test.result_path, exist_ok=True)
-    if rstFile is None:
-        rstFile = os.path.join(args.test.result_path, './original_TEST_det_{:s}.json'.format(args.test.day))         
-    else:
-        rstFile=rstFile + f'_{args.test.day}.json'
-    write_coco_format(results, rstFile)
-
-    try:
-        cocoGt = COCO(args.path.JSON_GT_FILE)
-        cocoDt = cocoGt.loadRes(rstFile)
-        imgIds = sorted(cocoGt.getImgIds())
-        cocoEval = COCOeval(cocoGt, cocoDt, 'bbox')
-        cocoEval.params.catIds  = [1]    
-        
-        cocoEval.params.imgIds  = imgIds
-        cocoEval.evaluate(0)
-        cocoEval.accumulate()
-        curPerf = cocoEval.summarize(0)     
-
-        cocoEval.draw_figure(ax_test, rstFile.replace('json', 'jpg'))        
-        
-        print('Recall: {:}'.format( 1-cocoEval.eval['yy'][0][-1] ) )
-        print('Time : ',data_time.avg)
-
-    except:
-        import utils.trace_error
-        print('[Error] cannot evaluate by cocoEval. ')
-
-def evaluate_matlab(test_loader, model, file_name):
-    """
-    Evaluate.
-
-    :param test_loader: DataLoader for test data
-    :param model: model
-    """
-    fig_test,  ax_test  = plt.subplots(figsize=(18,15))
-
-    # MLPD config
-    input_size = args.test.input_size
-    device = args.device
-
-    # Make sure it's in eval mode
-    model.eval()
-
-    # Lists to store detected and true boxes, labels, scores
-    det_boxes = list()
-    det_labels = list()
-    det_scores = list()
-    true_boxes = list()
-    true_labels = list()
-
-    #For CoCo
-    results = []
-
-    with torch.no_grad():
-        
-        if file_name is None:
-            file_name = os.path.join(anno_save, f'det-test-{args.test.day}')
-        f = open(file_name, 'w')
-
-        for i, (image_vis, image_lwir, boxes, labels, index) in enumerate(tqdm(test_loader, desc='Evaluating')):
+        for i, blob in enumerate(tqdm(test_loader, desc='Evaluating')):
+            image_vis, image_lwir, boxes, labels, indices = blob
 
             image_vis = image_vis.to(device)
             image_lwir = image_lwir.to(device)
 
             # Forward prop.
             predicted_locs, predicted_scores = model(image_vis, image_lwir)
-            
+
             # Detect objects in SSD output
-            det_boxes_batch, det_labels_batch, det_scores_batch, det_bg_scores_batch = model.detect_objects(predicted_locs, predicted_scores,
-                                                                                       min_score=0.1, max_overlap=0.45,
-                                                                                       top_k=200)
+            detections = model.module.detect_objects(predicted_locs, predicted_scores,
+                                       min_score=0.1, max_overlap=0.45, top_k=200)
 
-            for box_t, label_t, score_t, ids in zip(det_boxes_batch ,det_labels_batch, det_scores_batch, index):
-        
-                frame_id = img_set_list[ids.item()]
+            det_boxes_batch, det_labels_batch, det_scores_batch = detections[:3]
 
+            for boxes_t, labels_t, scores_t, image_id in zip(det_boxes_batch ,det_labels_batch, det_scores_batch, indices):
+                boxes_np = boxes_t.cpu().numpy().reshape(-1, 4)
+                scores_np = scores_t.cpu().numpy().mean(axis=1).reshape(-1, 1)
 
-                for box, label, score in zip(box_t, label_t, score_t) :
-                    
-                    bb = box.cpu().numpy().tolist()
-                    
-                    bbox = [bb[0]*input_size[1], bb[1]*input_size[0], (bb[2])*input_size[1], (bb[3])*input_size[0]]
-                    image_id = ids.item()
-                    score = score.mean().item()
-                    out_txt = str(image_id+1) + ',' + str(format(bbox[0],".4f")) + ',' + str(format(bbox[1],".4f")) + ',' + str(format(bbox[2]-bbox[0],".4f")) + ',' + str(format(bbox[3]-bbox[1],".4f")) + ',' + str(format(score,".8f")) + '\n'
-                    f.write(out_txt)
-        
-        f.close()
+                # TODO(sohwang): check if labels are required
+                labels_np = labels_t.cpu().numpy().reshape(-1, 1)
+
+                xyxy_np = boxes_np * xyxy_scaler_np
+                xywh_np = xyxy_np
+                xywh_np[:, 2] -= xywh_np[:, 0]
+                xywh_np[:, 3] -= xywh_np[:, 1]
+
+                results[image_id.item()+1] = np.hstack([xywh_np, scores_np])
+    return results
+
+def save_results(results: Dict, result_filename: str):
+    """Save detections
+
+    Write a result file (.txt) for detection results.
+    The results are saved in the order of image index.
+
+    Parameters
+    ----------
+    results: Dict
+        Detection results for each image_id: {image_id: box_xywh + score}
+    result_filename: str
+        Full path of result file name
+
+    """
+
+    if not result_filename.endswith('.txt'):
+        result_filename += '.txt'
+
+    with open(result_filename, 'w') as f:
+        for image_id, detections in sorted(results.items(), key=lambda x: x[0]):
+            for x, y, w, h, score in detections:
+                f.write(f'{image_id},{x:.4f},{y:.4f},{w:.4f},{h:.4f},{score:.8f}\n')
 
 
 if __name__ == '__main__':
 
+    FDZ_list = FusionDeadZone._FDZ_list
+
     parser = argparse.ArgumentParser(description='Process some integers.')
     
-    parser.add_argument('--FDZ', default='original',  type=str,
-                        help='Setting for the "Fusion Dead Zone" experiment. e.g. original, blackout_R, blackout_T, SidesBlackout_a, SidesBlackout_b, SurroundingBlackout')
+    parser.add_argument('--FDZ', default='original', type=str, choices=FDZ_list,
+                        help='Setting for the "Fusion Dead Zone" experiment. e.g. {}'.format(', '.join(FDZ_list)))
+    parser.add_argument('--model-path', type=str,
+                        help='Pretrained model for evaluation.')
+    parser.add_argument('--result-file', type=str,
+                        help='Detection result file for evaluation.')
+    arguments = parser.parse_args()
 
-    FDZ_CASE = parser.parse_args().FDZ.lower()
+    assert arguments.model_path or arguments.result_file, "Please specify '--model-path' or '--results-file'"
+    if arguments.model_path and arguments.result_file:
+        print(f'Both --model-path and --results-file are specified. Ignore --model-path.')
+        arguments.model_path = None
 
-    FDZ_list = ['original', 'blackout_r', 'blackout_t', 'sidesblackout_a', 'sidesblackout_b', 'surroundingblackout']
+    print(arguments)
 
-    test = args.test
-    
-    if FDZ_CASE not in FDZ_list:
-        raise NameError(f'{FDZ_CASE} is not support')
-    elif FDZ_CASE is not 'original':
-        print(f'Experiment {FDZ_CASE}')
-        rstFile = f'{test.result_path}/{FDZ_CASE}_TEST_det'
-    else:
-        rstFile = None # original
+    fdz_case = arguments.FDZ.lower()
+    model_path = arguments.model_path
+    result_filename = arguments.result_file
 
-    # Load model checkpoint that is to be evaluated
-    checkpoint = torch.load(test.checkpoint)
-    model = checkpoint['model']
-    model = model.to(args.device)
+    # Run inference to get detection results
+    if model_path:
+        result_filename = model_path + f'.{fdz_case}_TEST_det'
 
-    model = nn.DataParallel(model)
+        # Run inference
+        results = run_inference(model_path, fdz_case)
 
-    model.eval()
-
-    # Fusion Dead Zone experiment
-    FDZ = [FusionDeadZone(args.FDZ_case[FDZ_CASE], tuple(test.input_size)) ]
-    test.img_transform.add(FDZ)
-
-    test_dataset = KAISTPed(args, condition="test")
-
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=test.batch_size,
-                                                num_workers=args.dataset.workers,
-                                                collate_fn=test_dataset.collate_fn, 
-                                                pin_memory=True)     
+        # Save results
+        save_results(results, result_filename)
 
 
-    evaluate_coco(test_loader, model, rstFile)
+    # TODO(sohwang): Load results
+
+    # TODO(sohwang): Evaluate performance
